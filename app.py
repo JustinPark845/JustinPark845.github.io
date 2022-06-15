e532bde98adbcfd5bccd686fff6b80ff0540588e
Justin Park <79668170+rpcagithub@users.noreply.github.com>
2022-06-15 02:15:33 -0400

    update

--- app.py
+++
@@ -1,7 +0,0 @@
-from flask import Flask, redirect, render_template, request
-
-app = Flask(__name__)
-
-@app.route("/", methods=["GET", "POST"])
-def titlepage():
-    return render_template("index.html")
