371aa1a3952143b5523b93b7fc8b472d6cdfe61f
Justin Park <79668170+rpcagithub@users.noreply.github.com>
2022-06-14 23:02:56 -0400

    Directory Organization

---
+++ static/js/scripts.js
@@ -0,0 +1,13 @@
+/* scale content to page width */
+var w = window.innerWidth;
+var wscale = w / 1441;
+var hscale = (723/1441) * w;
+var wmarginsubtract = (1441 - w) / 2;
+var hmarginsubtract = (723 - hscale) / 2;
+document.getElementById("mainwindow").style.transform = 'scale('+wscale+')';
+document.getElementById("mainwindow").style.WebkitTransform = 'scale('+wscale+')';
+document.getElementById("mainwindow").style.MozTransform = 'scale('+wscale+')';
+document.getElementById("mainwindow").style.OTransform = 'scale('+wscale+')';
+document.getElementById("mainwindow").style.msTransform = 'scale('+wscale+')';
+document.getElementById("mainwindow").style.margin= '-'+hmarginsubtract+'px -'+wmarginsubtract+'px 0px -'+wmarginsubtract+'px';
+document.getElementById("disclaimer").style.top= hscale+'px';
